0fe1fedb4d437af33cb66f7c695ab4aabc85916f
Oishi Akito <66ed3gs@gmail.com>
2021-02-07 18:10:12 +0900

    add(theme): add radius foundations

--- src/theme/foundations/index.ts
+++ src/theme/foundations/index.ts
@@ -1,9 +1,11 @@
 import breakpoints from './breakpoints';
 import colors from './colors';
+import radii from './radius';
 
 const foundations = {
   breakpoints,
   colors,
+  radii,
 };
 
 export default foundations;
