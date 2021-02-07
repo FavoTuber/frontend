0780e314c99e9ff404266ce257a62504f02e2200
Oishi Akito <66ed3gs@gmail.com>
2021-02-07 18:56:17 +0900

    add(theme): link component

---
+++ src/theme/components/link.ts
@@ -0,0 +1,47 @@
+import { mode } from '@chakra-ui/theme-tools';
+
+type Dict = Record<string, any>;
+
+const baseStyle = {
+  _hover: {
+    textDecoration: 'none',
+  },
+};
+
+const sizes = {
+  default: {},
+  list: {
+    p: 8,
+  },
+};
+
+function defaultVariant(props: Dict): Dict {
+  return {
+    color: mode('text.light', 'text.dark')(props),
+    _hover: {
+      bgColor: mode('componentBg.light', 'componentBg.dark')(props),
+      borderRadius: 'base',
+    },
+    _focus: {
+      boxShadow: 'none',
+      bgColor: mode('componentBg.light', 'componentBg.dark')(props),
+      borderRadius: 'base',
+    },
+  };
+}
+
+const variants = {
+  default: defaultVariant,
+};
+
+const defaultProps = {
+  variant: 'default',
+  size: 'default',
+};
+
+export default {
+  baseStyle,
+  sizes,
+  variants,
+  defaultProps,
+};
