2454a78600d286e858e6e66a86c755f2bf2b4d50
Oishi Akito <66ed3gs@gmail.com>
2021-02-08 11:11:24 +0900

    change(story): change stories regex

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,10 @@ const path = require('path');
 const toPath = (_path) => path.join(process.cwd(), _path);
 
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../src/components/**/*.stories.mdx',
+    '../src/components/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   typescript: {
     check: false,
