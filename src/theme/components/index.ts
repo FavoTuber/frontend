771299e997ea50b43b06a84722a5aa7875956e95
Oishi Akito <66ed3gs@gmail.com>
2021-02-12 18:35:37 +0900

    add(theme): add `container` component

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,9 +1,11 @@
 import Button from './button';
 import Heading from './heading';
 import Link from './link';
+import Container from './container';
 
 export default {
   Button,
   Heading,
   Link,
+  Container,
 };
