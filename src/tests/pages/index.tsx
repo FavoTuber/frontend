275c1b6a59e87150021fa62ab98c74f83fc54996
Oishi Akito <66ed3gs@gmail.com>
2021-02-13 10:56:21 +0900

    add(page): create top page(beta) (#27)
    * add(theme): add `container` component
    
    * disable(test): disable `index` page test
    
    * change(page): create index page(beta)

--- src/tests/pages/index.tsx
+++ src/tests/pages/index.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 
 import Home from 'pages/';
 
-describe('Hello, world!', () => {
+describe('Top Page Test', () => {
   it('App shows "Hello, world!" in a <h1> tag', () => {
     const app = shallow(<Home />);
-    expect(app.find('h1').text()).toEqual('Hello, world!');
+    console.log(app.find('a'));
+    expect(app.find('a').at(0).prop('href')).toEqual('/video/sqrsVsjfBRY');
   });
 });
---
+++ src/theme/components/container.ts
@@ -0,0 +1,9 @@
+const baseStyle = {
+  w: '100%',
+  maxW: '100%',
+  px: ['12px', '12px', '4vw'],
+};
+
+export default {
+  baseStyle,
+};
