4eb7521cb53e61e0c3398481e95ea910a2bc17b8
Oishi Akito <66ed3gs@gmail.com>
2021-02-14 10:09:12 +0900

    add(theme): add styles in `button` component

--- src/theme/components/button.ts
+++ src/theme/components/button.ts
@@ -2,13 +2,27 @@ import { mode } from '@chakra-ui/theme-tools';
 
 type Dict = Record<string, any>; // eslint-disable-line @typescript-eslint/no-explicit-any
 
-const baseStyle = {};
+const baseStyle = {
+  mx: { base: 0, md: 2.5 },
+  my: { base: 2.5, md: 0 },
+  px: { base: 15, md: 6 },
+  py: { base: 6, md: 4 },
+};
 
 const sizes = {};
 
 function defaultVariant(props: Dict): Dict {
   return {
     color: mode('text.light', 'text.dark')(props),
+    _hover: {
+      bgColor: mode('componentBg.light', 'componentBg.dark')(props),
+      borderRadius: 'base',
+    },
+    _focus: {
+      boxShadow: 'none',
+      bgColor: mode('componentBg.light', 'componentBg.dark')(props),
+      borderRadius: 'base',
+    },
   };
 }
 
