c5dadb128bc90b77d41f1aa2800a2413c1e4537c
Oishi Akito <66ed3gs@gmail.com>
2021-02-14 09:25:19 +0900

    fix(component): image render problem before loading images (#34)
    * add(image): add `noimg.svg`
    
    * fix(component): fix image render problem
    
    * fix(component): fix same problem in `Logo`
    
    * change(component): change to `boxSize`
    
    * fix(component): change <Image> to <Img>

--- src/components/Card/Video.tsx
+++ src/components/Card/Video.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Link, Box, Image, Heading } from '@chakra-ui/react';
+import { Link, Box, Img, Heading, AspectRatio } from '@chakra-ui/react';
 import { Video } from 'models/video';
 
 const VideoCard: React.FC<Video> = ({ id, thumbnailUrl, title }) => {
   return (
     <Box w="full" overflow="hidden">
       <Link href={'/video/' + id}>
-        <Box>
-          <Image src={thumbnailUrl} alt={title} />
-        </Box>
+        <AspectRatio ratio={16 / 9}>
+          <Box>
+            <Img src={thumbnailUrl} alt={title} />
+          </Box>
+        </AspectRatio>
         <Box>
           <Heading size="card" noOfLines={2}>
             {title}
