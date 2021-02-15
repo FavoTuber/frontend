db89b4127380457eebcd12d330dcc0df6669ba0a
Oishi Akito <66ed3gs@gmail.com>
2021-02-15 11:44:38 +0900

    add(page): create 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container, Box, Heading, Img, Text } from '@chakra-ui/react';
+
+const NotFound: React.FC = () => {
+  return (
+    <Container centerContent>
+      <Box>
+        <Heading fontSize={['4em', '5em', '6em']}>404</Heading>
+      </Box>
+      <Box>
+        <Text>お探しのページは見つかりませんでした。</Text>
+      </Box>
+    </Container>
+  );
+};
+
+export default NotFound;
