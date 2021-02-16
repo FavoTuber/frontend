385cffe5bef7c1759d4b5195d467daa52f2e6297
Oishi Akito <66ed3gs@gmail.com>
2021-02-16 17:25:01 +0900

    add(theme): add `menu` component

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,11 +1,13 @@
 import Button from './button';
 import Heading from './heading';
 import Link from './link';
+import Menu from './menu';
 import Container from './container';
 
 export default {
   Button,
   Heading,
   Link,
+  Menu,
   Container,
 };
