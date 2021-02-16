8f89572f114cb91787c4f4c629daed844599bf05
Oishi Akito <66ed3gs@gmail.com>
2021-02-17 02:15:01 +0900

    add(component): add link in `Logo` component

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { Img, Flex, Heading } from '@chakra-ui/react';
+import { Flex, Link, Img, Heading } from '@chakra-ui/react';
 
 const Logo: React.FC = () => {
   return (
-    <Flex align="center" mx={6} my={{ base: 2, md: 0 }} p={6}>
-      <Img
-        boxSize="28px"
-        src="/logo/icon.svg"
-        pointerEvents="none"
-        _selection={{ bgColor: 'transparent' }}
-      />
-      <Heading size="headerNav" variant="headerNav">
-        FavoTuber
-      </Heading>
-    </Flex>
+    <Link href="/">
+      <Flex align="center" mx={6} my={{ base: 2, md: 0 }} p={6}>
+        <Img
+          boxSize="28px"
+          src="/logo/icon.svg"
+          pointerEvents="none"
+          _selection={{ bgColor: 'transparent' }}
+        />
+        <Heading size="headerNav" variant="headerNav">
+          FavoTuber
+        </Heading>
+      </Flex>
+    </Link>
   );
 };
 
