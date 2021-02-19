0f3ef32f6aa95bdb798a322bd34baa0e969019a6
Oishi Akito <66ed3gs@gmail.com>
2021-02-19 10:15:46 +0900

    add(component): create tooltip component

---
+++ src/components/Overlay/Tooltip.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Tooltip, AspectRatio, Box, Button, Icon } from '@chakra-ui/react';
+import { BiInfoCircle } from 'react-icons/bi';
+
+type Props = {
+  text: string;
+};
+
+const HelpText: React.FC<Props> = ({ text }) => {
+  return (
+    <Tooltip label={text} aria-label="A tooltip" placement="bottom">
+      <AspectRatio ratio={1}>
+        <Box borderRadius="circle">
+          <Button
+            _hover={{
+              bgColor: 'transparent',
+            }}
+          >
+            <Icon as={BiInfoCircle} fontSize={['1em', '1.05em', '1.4em']} />
+          </Button>
+        </Box>
+      </AspectRatio>
+    </Tooltip>
+  );
+};
+
+export default HelpText;
